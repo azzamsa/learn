572d69ece625a8daaa33b122e4bd38c07b253f04
azzamsa <me@azzamsa.com>
2022-11-03 10:53:37 +0700

    refactor(riddles): rock paper scissors

--- src/riddles/src/rust/src/codewars/eight_kyu/rps.rs
+++ src/riddles/src/rust/src/codewars/eight_kyu/rps.rs
@@ -8,15 +8,9 @@
 /// My solution.
 pub fn rps(p1: &str, p2: &str) -> &'static str {
     match (p1, p2) {
-        ("rock", "scissors") => "Player 1 won!",
-        ("scissors", "rock") => "Player 2 won!",
-        ("scissors", "paper") => "Player 1 won!",
-        ("paper", "scissors") => "Player 2 won!",
-        ("paper", "rock") => "Player 1 won!",
-        ("rock", "paper") => "Player 2 won!",
-        ("rock", "rock") => "Draw!",
-        ("scissors", "scissors") => "Draw!",
-        ("paper", "paper") => "Draw!",
+        ("rock", "scissors") | ("paper", "rock") | ("scissors", "paper") => "Player 1 won!",
+        ("scissors", "rock") | ("paper", "scissors") | ("rock", "paper") => "Player 2 won!",
+        ("scissors", "scissors") | ("paper", "paper") | ("rock", "rock") => "Draw!",
         (&_, _) => unreachable!(),
     }
 }
