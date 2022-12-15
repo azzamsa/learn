8b81b1a2b5e048313ae1318052eacd779a647efb
azzamsa <me@azzamsa.com>
2022-12-16 05:00:28 +0700

    fix(pokedex-vue): avoid blank page in `cypress v12`

--- src/pokedex-vue/cypress/e2e/locale.cy.ts
+++ src/pokedex-vue/cypress/e2e/locale.cy.ts
@@ -1,10 +1,8 @@
 describe('Locale Test', () => {
-  it('visits the about page', () => {
+  it('click the locale button changes the language', () => {
     cy.visit('/about')
     cy.contains(':nth-child(2) > .mt-4', 'Browse your favourite pokemon')
-  })
 
-  it('change the locales', () => {
     cy.get('.navbar-end > :nth-child(2)').click()
     cy.contains(':nth-child(2) > .mt-4', 'Cari pokemon kesukaanmu')
   })
