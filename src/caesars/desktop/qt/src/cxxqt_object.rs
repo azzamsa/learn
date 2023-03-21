0153152aae0401f69b62bb593f15ff8dbe2b29c5
azzamsa <me@azzamsa.com>
2023-03-21 08:16:06 +0700

    feat(caesars): encrypt

--- src/caesars/desktop/qt/src/cxxqt_object.rs
+++ src/caesars/desktop/qt/src/cxxqt_object.rs
@@ -1,6 +1,8 @@
 #[cxx_qt::bridge]
 mod my_object {
 
+    use nrot::{rot, rot_letter, Mode};
+
     unsafe extern "C++" {
         include!("cxx-qt-lib/qstring.h");
         type QString = cxx_qt_lib::QString;
@@ -25,10 +27,20 @@ mod my_object {
 
     impl qobject::Rot {
         #[qinvokable]
-        pub fn encrypt(&self, string: &QString) -> QString {
-            let result = format!("{string} is a secret");
-            dbg!(&result);
-            QString::from(&result)
+        pub fn encrypt(&self, plain: &QString) -> QString {
+            let rotation = 13; // common ROT rotation
+            let plain = plain.to_string();
+            let plain = plain.as_bytes();
+
+            let bytes_result = rot(Mode::Encrypt, plain, rotation);
+            let mut secret = format!("{}", String::from_utf8_lossy(&bytes_result));
+
+            if plain.len() == 1 {
+                let byte_result = rot_letter(Mode::Encrypt, plain[0], rotation);
+                secret = format!("{}", String::from_utf8_lossy(&[byte_result]));
+            };
+
+            QString::from(&secret)
         }
     }
 }
