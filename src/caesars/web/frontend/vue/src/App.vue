146b0c8a4f4e5d3b9d0335d086e2056177728415
azzamsa <me@azzamsa.com>
2023-06-04 17:29:36 +0700

    build(caesars): upgrade Vue dependencies

--- src/caesars/web/frontend/vue/src/App.vue
+++ src/caesars/web/frontend/vue/src/App.vue
@@ -3,17 +3,18 @@ import Footer from "./components/FooterItem.vue";
 import Banner from "./components/BannerItem.vue";
 import Dashboard from "./components/DashboardItem.vue";
 
-import { createClient, provideClient } from "@urql/vue";
+import { Client, provideClient, cacheExchange, fetchExchange } from "@urql/vue";
 
-const client = createClient({
-  url: "http://localhost:8000/graphql",
+const client = new Client({
+  url: "http://localhost:7000/graphql",
+  exchanges: [cacheExchange, fetchExchange],
 });
 
 provideClient(client);
 </script>
 
 <template>
-  <section class="bg-main min-h-screen px-2 pt-12 pb-6 md:px-5 md:pt-20">
+  <section class="bg-main min-h-screen px-2 pb-6 pt-12 md:px-5 md:pt-20">
     <header class="mx-auto max-w-lg">
       <h1 class="text-center text-5xl font-bold text-white">Caesar</h1>
     </header>
