4b81f4880203bc1652b830a31da1c40f3d4f07c6
azzamsa <me@azzamsa.com>
2023-06-06 00:18:54 +0700

    feat(rust-cicd): initial

---
+++ src/rust-cicd/src/main.rs
@@ -0,0 +1,18 @@
+use axum::{response::Html, routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(handler));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 7000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
+}
