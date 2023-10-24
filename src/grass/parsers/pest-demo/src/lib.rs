7ab7eafed4ad4c257a5dacdb84e30c5c151aa4c8
azzamsa <me@azzamsa.com>
2023-10-24 22:15:51 +0700

    feat(grass): use better error handling in `pest`

--- src/grass/parsers/pest-demo/src/lib.rs
+++ src/grass/parsers/pest-demo/src/lib.rs
@@ -13,29 +13,17 @@ pub enum Operator {
     Subtract,
 }
 
-fn calculate(raw_expr: &str) -> i32 {
-    let expr = parser::parse(raw_expr);
-    let lhs = if let Some(lhs) = expr.lhs {
-        lhs
-    } else {
-        panic!("Unrecogizeble number")
-    };
+fn calculate(raw_expr: &str) -> Result<i32, String> {
+    let expr = parser::parse(raw_expr)?;
 
-    let rhs = if let Some(rhs) = expr.rhs {
-        rhs
-    } else {
-        panic!("Unrecogizeble number")
-    };
+    let lhs = expr.lhs.ok_or("Invalid number".to_string())?;
+    let rhs = expr.rhs.ok_or("Invalid number".to_string())?;
 
-    dbg!(&lhs);
-    dbg!(&rhs);
     match expr.operator {
-        None => {
-            panic!("Unrecogizeble operator")
-        }
+        None => Err("Invalid operator".to_string()),
         Some(op) => match op {
-            Operator::Add => lhs + rhs,
-            Operator::Subtract => lhs - rhs,
+            Operator::Add => Ok(lhs + rhs),
+            Operator::Subtract => Ok(lhs - rhs),
         },
     }
 }
@@ -46,8 +34,24 @@ mod tests {
 
     #[test]
     fn calc() {
-        assert_eq!(calculate("1+2"), 3);
-        assert_eq!(calculate("3-2"), 1);
-        assert_eq!(calculate("3 - 2"), 1);
+        assert_eq!(calculate("1+2"), Ok(3));
+        assert_eq!(calculate("3-2"), Ok(1));
+        assert_eq!(calculate("3 - 2"), Ok(1));
+    }
+
+    #[test]
+    fn no_whitespace() {
+        assert_eq!(calculate("1+2"), Ok(3));
+        assert_eq!(calculate("3-2"), Ok(1));
+    }
+
+    #[test]
+    fn invalid_number() {
+        assert!(calculate("1+x").is_err());
+    }
+
+    #[test]
+    fn invalid_operator() {
+        assert!(calculate("1&2").is_err());
     }
 }
--- src/grass/parsers/pest-demo/src/parser.rs
+++ src/grass/parsers/pest-demo/src/parser.rs
@@ -8,27 +8,44 @@ use crate::Operator;
 #[grammar = "calc.pest"]
 struct Calculator;
 
-pub fn parse(raw_expr: &str) -> Expr {
+pub fn parse(raw_expr: &str) -> Result<Expr, String> {
     let mut lhs: Option<i32> = None;
     let mut rhs: Option<i32> = None;
     let mut operator: Option<Operator> = None;
 
     let pair = Calculator::parse(Rule::spec, raw_expr)
-        .unwrap()
+        .map_err(|e| format!("Parse error: {}", e))?
         .next()
-        .unwrap();
-    dbg!("{:#?}", &pair);
+        .ok_or("No pairs found")?;
 
     for piece in pair.into_inner() {
-        // dbg!("{:#?}", &piece);
         match piece.as_rule() {
-            Rule::lhs => lhs = Some(piece.as_str().parse().unwrap()),
-            Rule::rhs => rhs = Some(piece.as_str().parse().unwrap()),
+            Rule::lhs => {
+                lhs = Some(
+                    piece
+                        .as_str()
+                        .parse()
+                        .map_err(|e| format!("Failed to parse lhs: {}", e))?,
+                )
+            }
+            Rule::rhs => {
+                rhs = Some(
+                    piece
+                        .as_str()
+                        .parse()
+                        .map_err(|e| format!("Failed to parse rhs: {}", e))?,
+                )
+            }
             Rule::add => operator = Some(Operator::Add),
             Rule::subtract => operator = Some(Operator::Subtract),
-            _ => unreachable!(),
+            _ => unreachable!(), // This should not happen due to the grammar.
         }
     }
 
-    Expr { lhs, rhs, operator }
+    // Check if the necessary parts are found
+    if lhs.is_none() || rhs.is_none() || operator.is_none() {
+        return Err("Invalid expression: missing parts".to_string());
+    }
+
+    Ok(Expr { lhs, rhs, operator })
 }
