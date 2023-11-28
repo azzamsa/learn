f17831507e2937777de55cd24ecb4fab351f52f9
azzamsa <me@azzamsa.com>
2023-11-29 00:16:24 +0700

    feat(caesars): get rid of hardcoded UTC offset
    Thanks to the new `chrono` support in `tracing-subscriber`.
    
    - https://docs.rs/tracing-subscriber/latest/tracing_subscriber/fmt/time/struct.ChronoLocal.html
    - https://github.com/tokio-rs/tracing/pull/2690

--- src/caesars/web/backend/src/logger.rs
+++ src/caesars/web/backend/src/logger.rs
@@ -1,7 +1,6 @@
-use time::format_description::well_known::Rfc3339;
 use tracing_subscriber::{
     filter::{self},
-    fmt::{layer, time::OffsetTime},
+    fmt::{layer, time::ChronoLocal},
     prelude::*,
     registry,
 };
@@ -15,20 +14,15 @@ pub fn init(config: &Config) -> Result<(), crate::Error> {
         filter::LevelFilter::DEBUG
     };
 
-    let env_filter = filter::EnvFilter::builder()
-        .with_default_directive(log_level.into())
-        .from_env()?
+    let env_filter = filter::EnvFilter::new("")
+        .add_directive(log_level.into())
         .add_directive("sqlx::query=error".parse()?)
         .add_directive("hyper=warn".parse()?)
         .add_directive("reqwest=warn".parse()?);
 
-    let utc_offset_hour = config.utc_offset_hour;
     let fmt_layer = layer()
         .with_target(true)
-        .with_timer(OffsetTime::new(
-            time::UtcOffset::from_hms(utc_offset_hour, 0, 0).unwrap_or(time::UtcOffset::UTC),
-            Rfc3339,
-        ))
+        .with_timer(ChronoLocal::rfc_3339())
         .with_filter(env_filter);
 
     registry().with(fmt_layer).init();
