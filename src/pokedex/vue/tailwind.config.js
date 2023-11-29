ed8767bdcd0fbca891bc393ecf5ed3b6e12ce64e
azzamsa <me@azzamsa.com>
2023-11-29 09:33:11 +0700

    refactor(pokedex): replace `prettier` with `eslint`

--- src/pokedex/vue/tailwind.config.js
+++ src/pokedex/vue/tailwind.config.js
@@ -4,19 +4,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  daisyui: {
-    themes: [
-      {
-        cupcake: {
-          ...require('daisyui/src/theming/themes')['[data-theme=cupcake]'],
-          '--btn-text-case': 'none',
-        },
-        dark: {
-          ...require('daisyui/src/theming/themes')['[data-theme=dark]'],
-          '--btn-text-case': 'none',
-        },
-      },
-    ],
-  },
   plugins: [require('daisyui')],
 }
