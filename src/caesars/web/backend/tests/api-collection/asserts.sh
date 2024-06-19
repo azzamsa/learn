0442507bb71b2bcb81bf3cc8b2a31d73d8b25d14
azzamsa <me@azzamsa.com>
2024-06-19 18:04:43 +0700

    test(caesars): add api-collection

---
+++ src/caesars/web/backend/tests/api-collection/asserts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# Script to run all the `api-collection` assertions.
+#
+
+# Define an array with the directory names
+directories=("meta" "nrot")
+
+# Iterate over the array and run the hurl command for each directory
+for dir in "${directories[@]}"; do
+	hurl --test --glob "${dir}/**/*.hurl" --variables-file props/local
+done
