7fe43f907525b665f29cca019af7d1a768881035
Azzam S.A <noreply@azzamsa.com>
2024-08-13 06:12:57 +0700

    feat(pokedex-leptos): DaisyUI skeleton

--- src/pokedex/leptos/src/main.rs
+++ src/pokedex/leptos/src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     mount_to_body(|| {
         view! {
             <p class="text-red-900">"Hello, world!"</p>
+            <button class="btn btn-primary">Button</button>
         }
     })
 }
