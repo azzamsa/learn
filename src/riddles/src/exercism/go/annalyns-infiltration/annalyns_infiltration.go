203b308f2bd0be8cb14f3ed0b146d3e0c3ee4610
Azzam S.A <noreply@azzamsa.com>
2025-06-16 19:18:29 +0700

    feat(exercism/go): Annalyn's Infiltration

---
+++ src/riddles/src/exercism/go/annalyns-infiltration/annalyns_infiltration.go
@@ -0,0 +1,37 @@
+package annalyn
+
+// CanFastAttack can be executed only when the knight is sleeping.
+func CanFastAttack(knightIsAwake bool) bool {
+	return !knightIsAwake
+}
+
+// CanSpy can be executed if at least one of the characters is awake.
+func CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake bool) bool {
+	if knightIsAwake || archerIsAwake || prisonerIsAwake {
+		return true
+	}
+	return false
+}
+
+// CanSignalPrisoner can be executed if the prisoner is awake and the archer is sleeping.
+func CanSignalPrisoner(archerIsAwake, prisonerIsAwake bool) bool {
+	if !archerIsAwake && prisonerIsAwake {
+		return true
+	}
+	return false
+}
+
+// CanFreePrisoner can be executed if the prisoner is awake and the other 2 characters are asleep
+// or if Annalyn's pet dog is with her and the archer is sleeping.
+func CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent bool) bool {
+	if petDogIsPresent {
+		if !archerIsAwake {
+			return true
+		}
+	} else {
+		if !archerIsAwake && !knightIsAwake && prisonerIsAwake {
+			return true
+		}
+	}
+	return false
+}
