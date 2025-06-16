a297cf2792976811272b07c2f411a77350192965
Azzam S.A <noreply@azzamsa.com>
2025-06-16 19:21:31 +0700

    refactor(exercism/go): Annalyn's Infiltration
    Based on:
    - https://exercism.org/tracks/go/exercises/annalyns-infiltration/solutions/magahet
    
    Lesson learned:
    - Go has default values or Zero values. See
      https://go.dev/tour/basics/12. No need to explicitly return `false`.
    - Condition can be grouped

--- src/riddles/src/exercism/go/annalyns-infiltration/annalyns_infiltration.go
+++ src/riddles/src/exercism/go/annalyns-infiltration/annalyns_infiltration.go
@@ -7,31 +7,16 @@ func CanFastAttack(knightIsAwake bool) bool {
 
 // CanSpy can be executed if at least one of the characters is awake.
 func CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake bool) bool {
-	if knightIsAwake || archerIsAwake || prisonerIsAwake {
-		return true
-	}
-	return false
+	return knightIsAwake || archerIsAwake || prisonerIsAwake
 }
 
 // CanSignalPrisoner can be executed if the prisoner is awake and the archer is sleeping.
 func CanSignalPrisoner(archerIsAwake, prisonerIsAwake bool) bool {
-	if !archerIsAwake && prisonerIsAwake {
-		return true
-	}
-	return false
+	return !archerIsAwake && prisonerIsAwake
 }
 
 // CanFreePrisoner can be executed if the prisoner is awake and the other 2 characters are asleep
 // or if Annalyn's pet dog is with her and the archer is sleeping.
 func CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent bool) bool {
-	if petDogIsPresent {
-		if !archerIsAwake {
-			return true
-		}
-	} else {
-		if !archerIsAwake && !knightIsAwake && prisonerIsAwake {
-			return true
-		}
-	}
-	return false
+	return (petDogIsPresent && !archerIsAwake) || (!archerIsAwake && !knightIsAwake && prisonerIsAwake)
 }
