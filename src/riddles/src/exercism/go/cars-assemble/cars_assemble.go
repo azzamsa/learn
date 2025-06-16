c5f52e7465776d0e2d7e2ad721ed88ad9a387d41
Azzam S.A <noreply@azzamsa.com>
2025-06-16 22:14:08 +0700

    feat(exercism/go): cars assemble

---
+++ src/riddles/src/exercism/go/cars-assemble/cars_assemble.go
@@ -0,0 +1,20 @@
+package cars
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour.
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return (successRate / 100) * float64(productionRate)
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute.
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate) / 60)
+}
+
+// CalculateCost works out the cost of producing the given number of cars.
+func CalculateCost(carsCount int) uint {
+	group := carsCount / 10
+	remainder := carsCount % 10
+	return uint((group * 95_000) + (remainder * 10_000))
+}
