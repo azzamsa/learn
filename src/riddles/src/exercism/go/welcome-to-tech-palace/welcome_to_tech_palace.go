702375ab851b305b73b626496d65a80a0f437ccd
Azzam S.A <noreply@azzamsa.com>
2025-06-16 22:53:05 +0700

    feat(exercism/go): welcome to tech palace

---
+++ src/riddles/src/exercism/go/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -0,0 +1,21 @@
+package techpalace
+
+import (
+	"strings"
+)
+
+// WelcomeMessage returns a welcome message for the customer.
+func WelcomeMessage(customer string) string {
+	return "Welcome to the Tech Palace, " + strings.ToUpper(customer)
+}
+
+// AddBorder adds a border to a welcome message.
+func AddBorder(welcomeMsg string, numStarsPerLine int) string {
+	return strings.Repeat("*", numStarsPerLine) + "\n" + welcomeMsg + "\n" + strings.Repeat("*", numStarsPerLine)
+}
+
+// CleanupMessage cleans up an old marketing message.
+func CleanupMessage(oldMsg string) string {
+	// I prefer `oldMsg.replace()`
+	return strings.TrimSpace(strings.ReplaceAll(oldMsg, "*", ""))
+}
