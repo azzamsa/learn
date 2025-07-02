2441137a1ba99332b178756393b8746b3d980689
Azzam S.A <noreply@azzamsa.com>
2025-07-02 18:20:47 +0700

    refactor(exercism/go): jedliks toys (method)
    Simpler code

--- src/riddles/src/exercism/go/jedliks-toys/car.go
+++ src/riddles/src/exercism/go/jedliks-toys/car.go
@@ -1,7 +1,5 @@
 package jedlik
 
-import "fmt"
-
 // Car implements a remote controlled car.
 type Car struct {
 	speed        int
@@ -18,27 +16,4 @@ func NewCar(speed, batteryDrain int) *Car {
 		batteryDrain: batteryDrain,
 		battery:      100,
 	}
-}
-
-func (c *Car) Drive() {
-	// Can drive
-	if c.batteryDrain <= c.battery {
-		c.distance = c.distance + c.speed
-		c.battery = c.battery - c.batteryDrain
-	}
-
-}
-
-func (c Car) DisplayDistance() string {
-	return fmt.Sprintf("Driven %d meters", c.distance)
-}
-
-func (c Car) DisplayBattery() string {
-	return fmt.Sprintf("Battery at %d%%", c.battery)
-}
-
-func (c Car) CanFinish(trackDistance int) bool {
-	maxDistance := c.speed * (c.battery / c.batteryDrain)
-	return maxDistance >= trackDistance
-
 }
--- src/riddles/src/exercism/go/jedliks-toys/jedliks_toys.go
+++ src/riddles/src/exercism/go/jedliks-toys/jedliks_toys.go
@@ -1,18 +1,24 @@
 package jedlik
 
-// TODO: define the 'Drive()' method
+import "fmt"
 
-// TODO: define the 'DisplayDistance() string' method
+func (c *Car) Drive() {
+	// Can drive
+	if c.batteryDrain <= c.battery {
+		c.distance += c.speed
+		c.battery -= c.batteryDrain
+	}
 
-// TODO: define the 'DisplayBattery() string' method
+}
 
-// TODO: define the 'CanFinish(trackDistance int) bool' method
+func (c Car) DisplayDistance() string {
+	return fmt.Sprintf("Driven %d meters", c.distance)
+}
 
-// Your first steps could be to read through the tasks, and create
-// these functions with their correct parameter lists and return types.
-// The function body only needs to contain `panic("")`.
-// 
-// This will make the tests compile, but they will fail.
-// You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more 
-// functionality.
+func (c Car) DisplayBattery() string {
+	return fmt.Sprintf("Battery at %d%%", c.battery)
+}
+
+func (c Car) CanFinish(trackDistance int) bool {
+	return c.speed*(c.battery/c.batteryDrain) >= trackDistance
+}
