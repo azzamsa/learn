2e5a81fe2e8c4e2da898cc849a650231c5006ef1
Azzam S.A <noreply@azzamsa.com>
2025-07-24 20:58:59 +0700

    feat(exercism): expenses - first class function

---
+++ src/riddles/src/exercism/go/expenses/expenses.go
@@ -0,0 +1,70 @@
+package expenses
+
+import (
+	"fmt"
+)
+
+// Record represents an expense record.
+type Record struct {
+	Day      int
+	Amount   float64
+	Category string
+}
+
+// DaysPeriod represents a period of days for expenses.
+type DaysPeriod struct {
+	From int
+	To   int
+}
+
+// Filter returns the records for which the predicate function returns true.
+func Filter(in []Record, predicate func(Record) bool) []Record {
+	result := []Record{}
+	for _, record := range in {
+		if predicate(record) {
+			result = append(result, record)
+		}
+	}
+	return result
+}
+
+// ByDaysPeriod returns predicate function that returns true when
+// the day of the record is inside the period of day and false otherwise.
+func ByDaysPeriod(p DaysPeriod) func(Record) bool {
+	return func(record Record) bool {
+		return record.Day >= p.From && record.Day <= p.To
+	}
+}
+
+// ByCategory returns predicate function that returns true when
+// the category of the record is the same as the provided category
+// and false otherwise.
+func ByCategory(c string) func(Record) bool {
+	return func(record Record) bool {
+		return record.Category == c
+	}
+}
+
+// TotalByPeriod returns total amount of expenses for records
+// inside the period p.
+func TotalByPeriod(in []Record, p DaysPeriod) float64 {
+	total := 0.0
+	records := Filter(in, ByDaysPeriod(p))
+	for _, record := range records {
+		total += record.Amount
+	}
+	return total
+}
+
+// CategoryExpenses returns total amount of expenses for records
+// in category c that are also inside the period p.
+// An error must be returned only if there are no records in the list that belong
+// to the given category, regardless of period of time.
+func CategoryExpenses(in []Record, p DaysPeriod, c string) (float64, error) {
+	records := Filter(in, ByCategory(c))
+	if len(records) == 0 {
+		return 0.0, fmt.Errorf("unknown category %s", c)
+	}
+	total := TotalByPeriod(records, p)
+	return total, nil
+}
