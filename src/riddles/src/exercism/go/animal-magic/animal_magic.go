b2009b5fe96eaa130cd3803a89e9610df2ab45a7
Azzam S.A <noreply@azzamsa.com>
2025-07-24 21:28:40 +0700

    feat(exercism): animal-magic - randomness

---
+++ src/riddles/src/exercism/go/animal-magic/animal_magic.go
@@ -0,0 +1,33 @@
+package chance
+
+import "math/rand"
+
+// RollADie returns a random int d with 1 <= d <= 20.
+func RollADie() int {
+	return rand.Intn(20) + 1
+}
+
+// GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
+func GenerateWandEnergy() float64 {
+	return rand.Float64() * 12.0
+}
+
+// ShuffleAnimals returns a slice with all eight animal strings in random order.
+func ShuffleAnimals() []string {
+	animals := []string{
+		"ant",
+		"beaver",
+		"cat",
+		"dog",
+		"elephant",
+		"fox",
+		"giraffe",
+		"hedgehog",
+	}
+	// shuffling the slice put its elements into a random order
+	rand.Shuffle(len(animals), func(i, j int) {
+		animals[i], animals[j] = animals[j], animals[i]
+	})
+
+	return animals
+}
