d9f0a2397fffba1f7b8ed2130e4dd1b631155621
Azzam S.A <noreply@azzamsa.com>
2025-07-25 22:12:49 +0700

    refactor(exercism/go): Collatz Conjecture
    improve function naming.

--- src/riddles/src/exercism/go/collatz-conjecture/collatz_conjecture.go
+++ src/riddles/src/exercism/go/collatz-conjecture/collatz_conjecture.go
@@ -3,10 +3,10 @@ package collatzconjecture
 import "fmt"
 
 func CollatzConjecture(n int) (int, error) {
-	return run(n, 0)
+	return step(n, 0)
 }
 
-func run(n int, count int) (int, error) {
+func step(n int, count int) (int, error) {
 	if n < 1 {
 		return 0, fmt.Errorf("%d must be positive", n)
 	}
@@ -17,8 +17,8 @@ func run(n int, count int) (int, error) {
 	count++
 
 	if n%2 == 0 {
-		return run(n/2, count)
+		return step(n/2, count)
 	} else {
-		return run(3*n+1, count)
+		return step(3*n+1, count)
 	}
 }
